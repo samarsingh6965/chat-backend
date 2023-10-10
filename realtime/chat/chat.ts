41d41e169bb857fea940c6f6e6cfeaa098b95fef
Samar Raj <samar.suvaidyam@gmail.com>
2023-10-10 17:44:21 +0530

    add

--- realtime/chat/chat.ts
+++ realtime/chat/chat.ts
@@ -1,11 +1,12 @@
+import mongoose from 'mongoose';
 import { UserRealtimeModel, MessageModel } from '../../Models';
 export default (socket: any, io: any) => {
     socket.on('typing', async (data: any) => {
         let userto = await UserRealtimeModel.findOne({ userId: data.to }, { connectionId: 1 });
         let userfrom = await UserRealtimeModel.findOne({ userId: data.from }, { activeChat: 1 });
         if (userto) {
-            let d = await new MessageModel(data).save()
-            if(data.to === userfrom.activeChat){
+            const newObjectId = new mongoose.Types.ObjectId(data.to);
+            if(newObjectId.equals(userfrom.activeChat)){
                 io.to(userto.connectionId).emit('typing', socket.userInfo._id);
             }
         }
@@ -15,8 +16,8 @@ export default (socket: any, io: any) => {
         let userto = await UserRealtimeModel.findOne({ userId: data.to }, { connectionId: 1 });
         let userfrom = await UserRealtimeModel.findOne({ userId: data.from }, { activeChat: 1 });
         if (userto) {
-            let d = await new MessageModel(data).save()
-            if(data.to === userfrom.activeChat){
+            const newObjectId = new mongoose.Types.ObjectId(data.to);
+            if(newObjectId.equals(userfrom.activeChat)){
                 io.to(userto.connectionId).emit('stop_typing', socket.userInfo._id);
             }
         }
@@ -25,10 +26,10 @@ export default (socket: any, io: any) => {
     socket.on('message', async (data: any) => {
         let userto = await UserRealtimeModel.findOne({ userId: data.to }, { connectionId: 1 });
         let userfrom = await UserRealtimeModel.findOne({ userId: data.from }, { activeChat: 1 });
-        console.log('typ',userto,userfrom)
         if (userto) {
-            let d = await new MessageModel(data).save()
-            if(data.to === userfrom.activeChat){
+            const newObjectId = new mongoose.Types.ObjectId(data.to);
+            let d = await new MessageModel(data).save();
+            if(newObjectId.equals(userfrom.activeChat)){
                 io.to(userto.connectionId).emit('message', d);
             }else{
                 io.to(userto.connectionId).emit('notify', d);
