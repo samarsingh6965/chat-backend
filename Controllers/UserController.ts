0a642d65096087118f982a2e9f66b1523c8c4cac
Samar Raj <samar.suvaidyam@gmail.com>
2023-10-11 11:02:47 +0530

    add

--- Controllers/UserController.ts
+++ Controllers/UserController.ts
@@ -18,69 +18,52 @@ export default {
             response.somethingWentWrong(res)
         }
     },
-    // getUsers: async (req: any, res: any) => {
-    //     const { search } = req.query;
-    //     try {
-    //         const chatHistory = await MessageModel.distinct('from', {
-    //             $or: [
-    //                 { from: req.user.userId },
-    //                 { to: req.user.userId },
-    //             ],
-    //         });
-    //         const chatHistory2 = await MessageModel.distinct('to', {
-    //             $or: [
-    //                 { from: req.user.userId },
-    //                 { to: req.user.userId },
-    //             ],
-    //         });
-    //         const uniqueUserIds = [...new Set([...chatHistory, ...chatHistory2])];
-    //         const filter = {
-    //             status: "active",
-    //             _id: { $ne: req.user.userId,$in: uniqueUserIds},
-    //             $or: [
-    //                 { name: { $regex: search, $options: 'i' } },
-    //                 { username: { $regex: search, $options: 'i' } }
-    //             ]
-    //         };
-    //         const Users = await UserModel.find(filter, {
-    //             _id: 1,
-    //             name: 1,
-    //             profileImage: 1,
-    //             gender: 1,
-    //             username: 1
-    //         }).populate('profileImage', { _id: 1, url: 1, mimetype: 1 });
-    //         response.handleSuccess(res,Users??[], 'Users Fetched');
-    //     } catch (error) {
-    //         console.error(error);
-    //         response.somethingWentWrong(res);
-    //     }
-    // },
-    getUsers: async (req, res) => {
+    getUsers: async (req:any, res:any) => {
         const { search } = req.query;
         const userId = new mongoose.Types.ObjectId(req.user.userId);
         try {
             const chatHistory = await MessageModel.distinct('from', {
                 $or: [
-                    { from: userId },
-                    { to: userId },
+                    { from: req.user.userId },
+                    { to: req.user.userId },
                 ],
             });
-            const filter = {
-                status: "active",
-                _id: { $ne: userId, $in: chatHistory },
+            const chatHistory2 = await MessageModel.distinct('to', {
                 $or: [
-                    { name: { $regex: search, $options: 'i' } },
-                    { username: { $regex: search, $options: 'i' } }
-                ]
-            };
+                    { from: req.user.userId },
+                    { to: req.user.userId },
+                ],
+            });
+            const uniqueUserIds = [...new Set([...chatHistory, ...chatHistory2])];
+            let filter:any;
+            if(search){
+                filter = {
+                    status: "active",
+                    _id: { $ne: userId},
+                    $or: [
+                        { name: { $regex: search, $options: 'i' } },
+                        { username: { $regex: search, $options: 'i' } }
+                    ]
+                };
+            }else{
+                filter = {
+                    status: "active",
+                    _id: { $ne: userId, $in: uniqueUserIds },
+                    $or: [
+                        { name: { $regex: search, $options: 'i' } },
+                        { username: { $regex: search, $options: 'i' } }
+                    ]
+                };
+            }
+            
             const Users = await UserModel.find(filter, {
                 _id: 1,
                 name: 1,
                 profileImage: 1,
                 gender: 1,
-                username: 1
+                username: 1,
+                bio:1
             }).populate('profileImage', { _id: 1, url: 1, mimetype: 1 });
-    
             // Now, let's find the last message for each user
             const lastMessages = await MessageModel.aggregate([
                 {
@@ -101,16 +84,14 @@ export default {
                     }
                 },
             ]);
-    
             // Combine the user data and last messages
-            const usersWithLastMessages = Users.map(user => {
-                const lastMessage = lastMessages.find(message => message._id.equals(user._id));
+            const usersWithLastMessages = Users.map((user:any) => {
+                const lastMessage = lastMessages.find((message:any) => message._id.equals(user._id));
                 return {
                     ...user.toObject(),
                     lastMessage: lastMessage ? lastMessage.lastMessage : null
                 };
             });
-    
             response.handleSuccess(res, usersWithLastMessages, 'Users Fetched');
         } catch (error) {
             console.error(error);
