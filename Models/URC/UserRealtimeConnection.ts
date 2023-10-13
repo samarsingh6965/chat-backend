579c2b4bf9ff00c13f4fbbd11935d12c23050f74
Samar Raj <samar.suvaidyam@gmail.com>
2023-10-13 12:20:54 +0530

    add

--- Models/URC/UserRealtimeConnection.ts
+++ Models/URC/UserRealtimeConnection.ts
@@ -8,6 +8,7 @@ export default (connection:any)=>{
             default:null
         },
         connectionId:String,
+        activeChat:{type:String,default:null},
         status: {type:Boolean, default:true},
         disconnectedAt:{type:Date, default:null},
         lastActiveAt:{type:Date, default:Date.now},
--- realtime/chat/chat.ts
+++ realtime/chat/chat.ts
@@ -2,42 +2,51 @@ import mongoose from 'mongoose';
 import { UserRealtimeModel, MessageModel, NotificationModel } from '../../Models';
 export default (socket: any, io: any) => {
     socket.on('typing', async (data: any) => {
-        let userto = await UserRealtimeModel.findOne({ userId: data.to }, { connectionId: 1 });
-        if (userto) {
+        let userto = await UserRealtimeModel.findOne({ userId: data.to }, { connectionId: 1, activeChat: 1 });
+        if (userto.activeChat === data.from) {
             io.to(userto.connectionId).emit('typing', data);
         }
         // console.log('typing', data, userto,userfrom);
     })
     socket.on('stop_typing', async (data: any) => {
-        let userto = await UserRealtimeModel.findOne({ userId: data.to }, { connectionId: 1 });
-        if (userto) {
+        let userto = await UserRealtimeModel.findOne({ userId: data.to }, { connectionId: 1, activeChat: 1 });
+        if (userto.activeChat === data.from) {
             io.to(userto.connectionId).emit('stop_typing', socket.userInfo._id);
         }
         // console.log('stop_typing', data, userto,userfrom);
     })
     socket.on('message', async (data: any) => {
-        let userto = await UserRealtimeModel.findOne({ userId: data.to }, { connectionId: 1 });
-        if (userto) {
-            let d = await new MessageModel(data).save();
-            io.to(userto.connectionId).emit('message', d);
-        }
-    })
-    socket.on('notification', async (data: any) => {
-        let userto = await UserRealtimeModel.findOne({ userId: data.to }, { connectionId: 1 });
-        if (userto) {
+        let userto = await UserRealtimeModel.findOne({ userId: data.to }, { connectionId: 1, activeChat: 1 });
+        let newMessage = await new MessageModel(data).save();
+        if (userto.activeChat === data.from) {
+            io.to(userto.connectionId).emit('message', newMessage);
+        } else {
             let d = await NotificationModel.findOneAndUpdate(
                 {
-                    from:data.from,
-                    to:data.to,
+                    from: data.from,
+                    to: data.to,
                 },
                 {
-                    lastMessage:data._id,
-                    seen:false
+                    lastMessage: newMessage._id,
+                    seen: false
                 },
-                {new:true,upsert: true }
+                { new: true, upsert: true }
             );
-            io.to(userto.connectionId).emit('notification',d);
+            io.to(userto.connectionId).emit('notification', d);
             // console.log('notification',d);
         }
     })
+    socket.on('activeChat', async (data: any) => {
+        let d = await UserRealtimeModel.findOneAndUpdate(
+            {
+                userId: data.from,
+            },
+            {
+                activeChat: data.to,
+            },
+            { new: true }
+        );
+        // console.log('active Chat Updated',data,d);
+    })
+
 }
