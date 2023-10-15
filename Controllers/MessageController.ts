7957d85472017b2acdaae2a6f63d8ae9a2ec7e80
Samar Raj <samar.suvaidyam@gmail.com>
2023-10-15 09:53:57 +0530

    add

--- Controllers/MessageController.ts
+++ Controllers/MessageController.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import {MessageModel  } from "../Models/index";
 import ServerResponseClass from "../ServerResponse/ServerResponse";
 const response = new ServerResponseClass();
@@ -12,6 +13,21 @@ export default {
         } catch (error) {
             console.log("Exception", error);
             response.somethingWentWrong(res)
+        }
+    },
+    updateMessages: async (req: any, res: any) => {
+        try {
+            const { to ,from} = req.body;
+            const toId = new mongoose.Types.ObjectId(to)
+            const fromId = new mongoose.Types.ObjectId(from)
+            const updatedNotifications = await MessageModel.updateMany(
+                { to:toId,from:fromId },
+                { $set: { seen: true } }
+            );
+            response.handleSuccess(res, updatedNotifications, 'Messages updated Successfully');
+        } catch (error) {
+            console.log("Exception", error);
+            response.somethingWentWrong(res);
         }
     },
 }
--- Routers/MessageRouter.ts
+++ Routers/MessageRouter.ts
@@ -2,4 +2,5 @@ import express from "express";
 const MessageRouter = express.Router();
 import MessageController from "../Controllers/MessageController";
 MessageRouter.get('/getMessages', MessageController.getMessages);
+MessageRouter.put('/updateMessages', MessageController.updateMessages);
 export default MessageRouter;
